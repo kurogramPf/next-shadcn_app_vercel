50001d05e9bbc33779993fac7b8e24629de804ec
kurogram0-r <kurogram0.r@gmail.com>
2025-05-04 05:35:21 +0900

    next-vercel-basic-authentication

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { withAuth } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export default withAuth(
   async function middleware(req) {
@@ -42,5 +42,42 @@ export default withAuth(
 
 export const config = {
   // :pathは/dashboard/配下が対象となる
-  matcher: ["/dashboard/:path", "/editor/:path", "/login", "/register"],
+  matcher: [
+    "/dashboard/:path",
+    "/editor/:path",
+    "/login",
+    "/register",
+    "/:path*",
+  ],
 };
+
+export function middleware(req: NextRequest) {
+  const isLocalDevelopment = process.env.NODE_ENV === "development";
+
+  if (isLocalDevelopment || !process.env.BASIC_ID || !process.env.BASIC_PWD) {
+    return NextResponse.next();
+  }
+
+  const basicAuth = req.headers.get("authorization");
+  if (!basicAuth) {
+    return new Response("Authentication required", {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Secure Area"',
+      },
+    });
+  }
+
+  try {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
+
+    if (user === process.env.BASIC_ID && pwd === process.env.BASIC_PWD) {
+      return NextResponse.next();
+    }
+  } catch (e) {
+    return new Response("Invalid Authentication", { status: 400 });
+  }
+
+  return new Response("Unauthorized", { status: 401 });
+}
